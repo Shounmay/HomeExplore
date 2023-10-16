aa4f9a13a72bd8eb94e803685ec280e6ab8878b9
Shounmay <shounmaydarsh131@gmail.com>
2023-10-17 03:10:50 +0530

    updated env for client side v1.0

---
+++ client/src/config.js
@@ -0,0 +1,3 @@
+export const API = process.env.REACT_APP_API;
+export const GOOGLE_PLACES_KEY = process.env.REACT_APP_GOOGLE_PLACES_KEY;
+export const GOOGLE_MAPS_KEY = process.env.REACT_APP_GOOGLE_MAPS_KEY;
